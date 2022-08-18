cf906a1dc195f7e2916993745d2199c19e4c9534
Gordon Beeming <gordon@devstarops.com>
2022-08-19 01:00:40 +0200

    install powershell and az cli

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,25 +6,32 @@ while [ "$(hostname -I)" = "" ]; do
 done
 echo "I have network";
 
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO="bullseye" # $(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install -y azure-cli
+
+
 DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq
 DEBIAN_FRONTEND=dialog
 
-getLatestVersion() {
-LATEST_ARR=($(wget -q -O- https://api.github.com/repos/hashicorp/terraform/releases 2> /dev/null | awk '/tag_name/ {print $2}' | cut -d '"' -f 2 | cut -d 'v' -f 2))
-for ver in "${LATEST_ARR[@]}"; do
-  if [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]]; then
-    LATEST="$ver"
-    break
-  fi
-done
-echo -n "$LATEST"
-}
-
-VERSION=$(getLatestVersion)
-
 cd ~
-wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
-unzip "terraform_"$VERSION"_linux_amd64.zip"
+wget "https://releases.hashicorp.com/terraform/1.2.4/terraform_1.2.4_linux_amd64.zip"
+unzip "terraform_1.2.4_linux_amd64.zip"
 sudo install terraform /usr/local/bin/
+
