35041d285dc83e5f0148643ae44bba2eac93467c
Gordon Beeming <gordon@devstarops.com>
2022-08-19 01:05:24 +0200

    Setup files needed for demo'ing in codespaces (#1)
    * dev container
    
    * install powershell and az cli
    
    * tasks for terraform tasks in codespaces

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+while [ "$(hostname -I)" = "" ]; do
+  echo -e "\e[1A\e[KNo network: $(date)"
+  sleep 1
+done
+echo "I have network";
+
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO="bullseye" # $(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install -y azure-cli
+
+
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq
+DEBIAN_FRONTEND=dialog
+
+cd ~
+wget "https://releases.hashicorp.com/terraform/1.2.4/terraform_1.2.4_linux_amd64.zip"
+unzip "terraform_1.2.4_linux_amd64.zip"
+sudo install terraform /usr/local/bin/
+
