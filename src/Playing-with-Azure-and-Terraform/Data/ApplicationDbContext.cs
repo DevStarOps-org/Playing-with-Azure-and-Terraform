7d523a84ae18c5a3c85c6fe468c4cf72a46c4174
Gordon Beeming <gordon@devstarops.com>
2022-08-19 04:16:13 +0200

    demo app

---
+++ src/Playing-with-Azure-and-Terraform/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Playing_with_Azure_and_Terraform.Data
+{
+  public class ApplicationDbContext : IdentityDbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+  }
+}
---
+++ src/Playing-with-Azure-and-Terraform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Playing_with_Azure_and_Terraform.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
